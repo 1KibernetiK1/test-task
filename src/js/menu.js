070990aae2e87a80aa6ec7c2c961e05edaeb68d8
1KibernetiK1 <123451040o@gmail.com>
2024-09-09 16:49:35 +0500

    Почти сверстал фрейм и сделал модальное окно

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,22 +1,119 @@
-  /*открываю попап*/
-  $(document).ready(function() { 
-	$('a#start').click( function(event){ 
-		event.preventDefault(); 
-		$('#overlay').fadeIn(250, 
-		 	function(){
-				$('#popUp') 
-					.css('display', 'block') 
-					.animate({opacity: 1, top: '55%'}, 490); 
-		});
-	});
-/*по нажатию на крестик или на тень закрываю окно*/
-	$('#close, #overlay').click( function(){ 
-		$('#popUp')
-			.animate({opacity: 0, top: '35%'}, 490, 
-				function(){ 
-					$(this).css('display', 'none'); 
-					$('#overlay').fadeOut(220); 
-				}
-			);
-	});
-});
+//init after DOM
+document.addEventListener('DOMContentLoaded', (e) => {
+	openModal()
+	formModule()
+})
+
+const formModule = () => {
+	const form = document.querySelector('.form')
+	const emailField = document.querySelector('.email_field')
+	const emailInput = document.querySelector('.email')
+	const errorEmail = document.querySelector('.error_mail')
+	const nameField = document.querySelector('.name_field')
+	const nameInput = document.querySelector('.name')
+	const errorName = document.querySelector('.error_name')
+	const success = document.querySelector('.success')
+
+	function checkEmail() {
+		const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+		if (emailInput?.value?.length < 3) {
+			return (
+				errorEmail.innerHTML = `<p>Too shorten email</p>`,
+				emailInput.style.border = '1px solid red'
+			)
+		}
+		if (!emailInput.value.match(emailPattern)) {
+			return (
+				errorEmail.innerHTML = `<p>Please enter a valid email</p>`,
+				emailInput.style.border = '1px solid red'
+			)
+		}
+		errorEmail.innerHTML = ''
+		emailInput.style.border = '1px solid'
+	}
+
+	function checkName(e) {
+		const minNameLength = 2
+		const namePattern = /^[a-zA-Z ]*$/
+		if (nameInput && nameInput.value <= minNameLength) {
+			return (
+				errorName.innerHTML = `<p>Too shorten name</p>`,
+				nameInput.style.border = '1px solid red'
+			)
+		}
+		if (!nameInput.value.match(namePattern)) {
+			return (
+				errorName.innerHTML = `<p>Please enter only text</p>`,
+				nameInput.style.border = '1px solid red'
+			)
+		}
+		errorName.innerHTML = ''
+		nameInput.style.border = '1px solid'
+	}
+
+	function submitForm() {
+
+		//Формируем данные для отправки на бек
+		const data = {
+			email: emailInput.value,
+			name: nameInput.value,
+		}
+		//Имитируем проверку на отсутствие ошибок
+		if (!errorEmail.hasChildNodes() && !errorName.hasChildNodes()) {
+			success.style.display = 'block'
+			success.innerHTML = 'Successfully sent'
+			form.style.display = 'none'
+		}
+
+	}
+
+	success.addEventListener("click", function () {
+		success.style.display = 'none'
+		form.style.display = 'block'
+		emailInput.value = ''
+		nameInput.value = ''
+	})
+
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault()
+		checkEmail()
+		checkName(e)
+		submitForm()
+	})
+
+}
+
+
+
+const openModal = () => {
+
+	const element = document.querySelector(".open_modal")
+	const body = document.querySelector('body')
+	const close = document.querySelector(".modal_close")
+	const modalBlock = document.querySelector(".modal_content")
+
+	const closeModal = () => {
+		document.querySelector(".modal").style.display = "none"
+	}
+
+	element.addEventListener("click", function () {
+		document.querySelector(".modal_js").style.display = "block"
+		body.classList.add('overflow-hidden')
+	})
+
+	close.addEventListener("click", function () {
+		closeModal()
+		body.classList.remove('overflow-hidden')
+	})
+
+	window.onclick = function (event) {
+		if (event.target.contains(modalBlock) && event.target !== modalBlock) {
+			closeModal()
+		}
+	}
+
+}
+
+
+
